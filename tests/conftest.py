2038b6b56912d953e2b4ce52632c86b24e200ddd
JGageWright <83935187+JGageWright@users.noreply.github.com>
2023-10-13 16:23:45 -0700

    Remove stuff

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.fixture(scope="session")
-def data_files_dir(tmpdir_factory):
-    datadir = tmpdir_factory.mktemp("data")
-    # create_data_files(str(datadir))
-    return datadir
